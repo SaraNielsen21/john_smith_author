5343edd86f60e5af5c4636629d0f41b1d79b64c2
SARA-PC\Sara <sara.nielsen@my.simpson.edu>
2016-11-17 15:36:15 -0600

    Amazon login

--- public_html/login.php
+++ public_html/login.php
@@ -12,6 +12,21 @@
     
     <body>
 
+    <div id="amazon-root"></div>
+<script type="text/javascript">
+
+  window.onAmazonLoginReady = function() {
+    amazon.Login.setClientId('amzn1.application-oa2-client.61cb7df373a44c39800bdfa39d871e72');
+  };
+  (function(d) {
+    var a = d.createElement('script'); a.type = 'text/javascript';
+    a.async = true; a.id = 'amazon-login-sdk';
+    a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
+    d.getElementById('amazon-root').appendChild(a);
+  })(document);
+
+</script>
+
         <?php include '../resources/navigation.php'; ?>
 
         <div class="container">
@@ -24,6 +39,26 @@
                 Password:<br>
                 <input type="text" class="form-control" name="password" placeholder="Password"><br/>
                 <input type="submit" class="btn btn-primary submit" value="Submit">
+                <a href="#" id="LoginWithAmazon">
+  <img border="0" alt="Login with Amazon"
+    src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+    width="156" height="32" />
+</a>
+
+<script type="text/javascript">
+
+  document.getElementById('LoginWithAmazon').onclick = function() {
+    options = { scope : 'profile' };
+    amazon.Login.authorize(options, 'https://ec2-35-160-178-198.us-west-2.compute.amazonaws.com/handle_login.php');
+    return false;
+  };
+
+</script>
+<script type="text/javascript">
+  document.getElementById('Logout').onclick = function() {
+    amazon.Login.logout();
+};
+</script>
               </div>     
             </form>
           </div>
